6f7412f5e3e8b98cde3d4eb16f42e13a136dc2f9
lucas felix <65676345+Lucasfelix1017@users.noreply.github.com>
2020-07-03 12:20:17 -0300

    Add files via upload
    operações básicas de matemática curso de C youtube
    
    eXcript vídeo de numero 11

---
+++ 4 operaçoes basicas de matematica.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	int num1, num2;
+	num1 =10;
+	num2= 20;
+	
+//soma um numero com outro
+	int soma = num1 + num2;
+	printf(" o resutado da soma  %i\n" , soma);
+	
+// faz a subtração de dois numeros
+	int subtracao = num1 - num2;
+	printf("o resultado da subtracao %i\n", subtracao);
+
+// faz a multiplicação de dois numeros 
+    int mult = num1 * num2;
+    printf("o resultado da multiplicacao %i\n", mult);
+     
+
+// faz a divisão de dois numeros 
+	int div = num1 / num2;
+	printf ("o resultado da divisã %i\n", div);
+	  
+
+	
+	
+	return 0;
+}
