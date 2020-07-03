577404f22868b88c94060ed23bd819e6af474460
lucas felix <65676345+Lucasfelix1017@users.noreply.github.com>
2020-07-03 12:33:32 -0300

    Add files via upload
    lê uma informação do cliente

---
+++ ler na tela um numero.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+	
+// scanf("tipo de entrada", variavel que recebera);	
+// scanf ("%tipo1%tipo2" &var1, &var2);
+	int num1;
+	printf("digite um numero:\n");
+	scanf("%i", &num1);
+	
+	printf("%i", num1);
+	
+	
+	
+	
+	return 0;
+	
+}
